5f365463bc863a1e3616e4c474f054e93bb51cf4
JungGyeongmi <gm950715@gmail.com>
2023-12-09 17:44:34 +0900

    20231209 5052

---
+++ CodingTest/CodingTest/AppStart.cs
@@ -0,0 +1,14 @@
+﻿using CodingTest.Backjoon;
+using System;
+
+namespace CodingTest
+{
+    class AppStart
+    {
+        static void Main(string[] args)
+        {
+            _5052 target = new _5052();
+            target.Program();
+        }
+    }
+}
---
+++ CodingTest/CodingTest/Backjoon/_5052.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest.Backjoon
+{
+    internal class _5052
+    {
+        public void Program()
+        {
+            int cnt = int.Parse(Console.ReadLine());
+            int maxCnt;
+            bool trigger;
+            string[] numbers;
+
+            for (int i = 0; i < cnt; i++)
+            {
+                trigger = false;
+                numbers = new string[maxCnt = int.Parse(Console.ReadLine())];
+
+                for (int j = 0; j < maxCnt; j++)
+                {
+                    numbers[j] = Console.ReadLine();
+                }
+
+                Array.Sort(numbers);
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    Console.WriteLine("j " + numbers[j]); 
+                    Console.WriteLine("j + 1 "+numbers[j + 1]); 
+                    
+                    if (numbers[j + 1].StartsWith(numbers[j]))
+                    {
+                        trigger = true;
+                        // break;
+                        Console.WriteLine("Y");
+                    }
+                    
+                    Console.WriteLine();
+                }
+                Console.WriteLine(trigger ? "NO" : "YES");
+            }
+        }
+    }
+}
